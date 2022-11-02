e82ed1139c807ada6a1226b955b8818b3e8bd3fd
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-11-02 23:06:12 +0000

    Making Program structure

--- index.js
+++ index.js
@@ -1,44 +1,72 @@
 var fs = require('fs');
 var path = require('path');
+const readline = require('readline');
+const { once } = require('node:events');
 
 let snippetsFolder = path.join(__dirname, "snippets")
 
-let loadFile = () =>{
+let loadedCount = 0
+let fileCount = 0
 
+function FinishProgram(){
+    console.log("Finished program");
 }
 
-fs.readdir(snippetsFolder, (err, files)=>{
-    files.forEach((file, index)=>{
-        let fullFilePath = path.join(snippetsFolder, file)
-        let words = file.split(".")
-        let fileContent = ""
-        let fileType = words[words.length - 1]
-        let fileName = words[0]
-        
-        fileContent = fs.readFileSync(fullFilePath, 'utf8')
+async function readFile(fullFilePath){
+    let fileContent = readline.createInterface({
+        input: fs.createReadStream(fullFilePath)
+    });
 
-        let finalString = ""
+    let cleanedContent = ""
+    fileContent.on('line', (line) => {
+        cleanedContent += line
+    })
 
-        let prefixes = ""
-        for(let i = 0; i < words.length; i++){
-            if (i == words.length - 1) {
-                continue
-            }
+    await once(fileContent, 'close');
 
-            if(i > 0){
-                prefixes += ","
-            }
-            prefixes += '"'+words[i]+'"'
+    return cleanedContent
+}
+
+async function loadFile(file){
+    let fullFilePath = path.join(snippetsFolder, file)
+    let words = file.split(".")
+    let fileType = words[words.length - 1]
+    let fileName = words[0]
+
+    let finalString = ""
+
+    let prefixes = ""
+    for(let i = 0; i < words.length; i++){
+        if (i == words.length - 1) {
+            continue
         }
 
-        let cleanedContent = ""
-        
-        finalString += `"${fileName}": {\n`
-        finalString += `\t"scope": "${fileType}",\n` //Scope
-        finalString += `\t"prefix": [${prefixes}],\n` //Prefixes
-        finalString += `\t"body":["${cleanedContent}"],\n`
-        finalString += `\t"description": "No"\n`
-        finalString += "}"
-        console.log(finalString)
-    })
+        if(i > 0){
+            prefixes += ","
+        }
+        prefixes += '"'+words[i]+'"'
+    }
+
+
+    let cleanedContent = await readFile(fullFilePath)      
+    console.log(cleanedContent)
+    
+    finalString += `"${fileName}": {\n`
+    finalString += `\t"scope": "${fileType}",\n` //Scope
+    finalString += `\t"prefix": [${prefixes}],\n` //Prefixes
+    finalString += `\t"body":["${cleanedContent}"],\n`
+    finalString += `\t"description": "No"\n`
+    finalString += "}"
+
+    loadedCount += 1
+    if(loadedCount == fileCount){
+        FinishProgram();
+    }
+}
+
+let files = fs.readdirSync(snippetsFolder)
+
+files.forEach((file)=>{
+    fileCount += 1
+    loadFile(file)
 })

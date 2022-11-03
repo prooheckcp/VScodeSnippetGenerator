0bc4d0f7289986a3b2aaa78c68b83ea2131e3265
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-11-03 18:45:53 +0000

    Creating roact element

---
+++ snippets/Roact/roactelement.element.lua
@@ -0,0 +1,10 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Roact = require(ReplicatedStorage.Packages.roact)
+local Hooks = require(ReplicatedStorage.Packages["roact-hooks"])
+
+local function $1(props)
+
+end
+
+return $1
--- snippets/roact/test.uwu.lua
+++
@@ -1,3 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Workspace = game:GetService("Workspace")
-local Workspace = game:GetService("Workspace")

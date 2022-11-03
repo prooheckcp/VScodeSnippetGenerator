a3f52948a8b9548f1bd48a094df9a97cac8b55ce
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-11-03 18:49:57 +0000

    Adding enum

---
+++ snippets/util/enum.lua
@@ -0,0 +1,9 @@
+local ${1:enum} = {
+
+}
+
+table.freeze(${1:enum})
+
+export type ${1:enum} = typeof(${1:enum})
+
+return ${1:enum} 
---
+++ test.lua
@@ -0,0 +1,9 @@
+local CharacterState = {
+
+}
+
+table.freeze(CharacterState )
+
+export type CharacterState = typeof(CharacterState )
+
+return CharacterState 

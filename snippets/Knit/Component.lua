103c38711acc80c3fd90a0bf00bf576e4002b0e1
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-11-03 18:46:59 +0000

    Adding knit component

---
+++ snippets/Knit/Component.lua
@@ -0,0 +1,9 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Component = require(ReplicatedStorage.Packages.component)
+
+local $1 = Component.new({
+    Tag = "$1",
+})
+
+return $1
